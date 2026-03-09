728246973b952c245b0bf2bfb6be8589cb79443b
pandariot <peggy@buzzfeed.com>
2026-03-08 23:09:31 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/hudson">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      <BrowserRouter basename="/hudson">
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
